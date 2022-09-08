cf8736458c5d7e49b9ed6d91fff83b4400490af5
oussenimbohou <oussenimbohou@gmail.com>
2022-09-08 05:01:44 -0700

    Task4 and Task 5 done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
+  2 /**
+  3  * main - Entry point
+  4  *
+  5  * Return: Always 0 (Success)
+  6  */
+  7 int main(void)
+  8 {
+  9         puts("\"Programming is like building a multilingual puzzle    ");
+ 10         return (0);
+ 11 }
 
-/*
- * main: Entry point.
- * Retun: Always 0 if successful
-*/
-
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,11 @@
-#include <stdio.h>
+  1 #include <stdio.h>
+  2 /**
+  3  * main - Entry point
+  4  *
+  5  * Return: Always 0 (Success)
+  6  */
+  7 int main(void)
+  8 {
+  9         printf("with proper grammar, but the outcome is a piece of     art,\n");
+ 10         return (0);
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-
-int main(void)
-{
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
-}
