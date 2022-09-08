2705fd911ab1e99b23b499f29065eabbab4ad547
oussenimbohou <oussenimbohou@gmail.com>
2022-09-08 04:07:17 -0700

    Fixing Task 2,Task 4 to Task 6

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,11 @@
 #include<stdio.h>
-int main(void){
+
+/*
+ * main: Entry point
+ * Return(0): When the program is successful.
+ */
+int main(void)
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return 0;
+	return(0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,13 @@
 #include<stdio.h>
+/*
+ * main: Starting point
+ * Return 0 if the program end with any error and non zero otherwise.
+ * */
 int main(void){
 	printf("Size of a char: %u byte(s)\n",sizeof(char));
 	printf("Size of an int: %u byte(s)\n",sizeof(int));
-	printf("Size of a long int: %u byte(s)\n",sizeof(long));
-	printf("Size of a long long: %u byte(s)\n",sizeof(long long));
+	printf("Size of a long int: %u byte(s)\n",sizeof(long int));
+	printf("Size of a long long: %u byte(s)\n",sizeof(long long int));
 	printf("Size of a float: %u byte(s)\n",sizeof(float));
-	return 0;
+	return (0);
 }
