06df8ed4101cbe1a7edb4f810f1f8c0987f6ed98
oussenimbohou <oussenimbohou@gmail.com>
2022-09-09 14:28:28 -0700

    Finally task 102 done

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,44 +1,41 @@
 #include<stdio.h>
 /**
-  *main - Print out the combination of 4 digits.
+  *main - Printing the combination of 4 digits
   *
   *Return: Always 0 (success)
   *
   */
 int main(void)
 {
-        int second_digit;
-        int first_digit;
-        int t;
-        int o;
+	int tens;
+	int ones;
+	int t;
+	int o;
 
-        for (second_digit = '0'; second_digit <= '9'; second_digit++) /*print first two digit combo*/
-        {
-                for (first_digit = '0'; first_digit <= '9'; first_digit++)
-                {
-                        for (t = second_digit; t <= '9'; t++) /*print second of pair*/
-                        {
-                                for (o = first_digit + 1; o <= '9'; o++)
-                                {
-                                        putchar(second_digit);
-                                        putchar(first_digit);
-                                        putchar(' ');
-                                        putchar(t);
-                                        putchar(o);
-
-                                        if (!((second_digit == '9' && first_digit == '8') &&
-                                              (t == '9' && o == '9')))
-                                        {
-                                                putchar(',');
-                                                putchar(' ');
-                                        }
-                                }
-                                o = '0';
-                        }
-                }
-        }
-        putchar('\n');
-
-        return (0);
+	for (tens = '0'; tens <= '9'; tens++)
+	{
+		for (ones = '0'; ones <= '9'; ones++)
+		{
+			for (t = tens; t <= '9'; t++)
+			{
+				for (o = ones + 1; o <= '9'; o++)
+				{
+					putchar(tens);
+					putchar(ones);
+					putchar(' ');
+					putchar(t);
+					putchar(o);
+					if (!((tens == '9' && ones == '8') &&
+							(t == '9' && 0 == '9')))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				o = '0';
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
-
