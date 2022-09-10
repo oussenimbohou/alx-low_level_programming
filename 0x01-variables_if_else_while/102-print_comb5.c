26519425eac8986378c9f6b72afdc48ed259c0e6
oussenimbohou <oussenimbohou@gmail.com>
2022-09-09 19:36:25 -0700

    Task 102 done

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,36 +9,23 @@ int main(void)
 {
 	int tens;
 	int ones;
-	int t;
-	int o;
 
-	for (tens = '0'; tens <= '9'; tens++)
+	for (tens = 0; tens <= 98; tens++)
 	{
-		for (ones = '0'; ones <= '9'; ones++)
+		for (ones = tens + 1; ones <= 99; ones++)
 		{
-			for (t = tens; t <= '9'; t++)
+			putchar((tens / 10) + '0');
+			putchar((tens % 10) + '0');
+			putchar(' ');
+			putchar((ones / 10) + '0');
+			putchar((ones % 10) + '0');
+			if (!(tens == 98 && ones == 99))
 			{
-				for (o = ones; o <= '9'; o++)
-				{
-					if (!(tens == ones && ones == t && t == o))
-					{
-						putchar(tens);
-						putchar(ones);
-						putchar(' ');
-						putchar(t);
-						putchar(o);
-						if (!(tens == '9' && ones == '8') &&
-							(t == '9' && o == '9'))
-						{
-							putchar(',');
-							putchar(' ');
-						}
-					}
-				}
-				o = '0';
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
-	putchar('\n');
+	putchar('\0');
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/test_file_102.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+/**
+  *main - Printing the combination of 4 digits
+  *
+  *Return: Always 0 (success)
+  *
+  */
+int main(void)
+{
+	int tens;
+	int ones;
+	int t;
+	int o;
+
+	for (tens = '0'; tens <= '9'; tens++)
+	{
+		for (ones = '0'; ones <= '9'; ones++)
+		{
+			for (t = tens; t <= '9'; t++)
+			{
+				for (o = ones; o <= '9'; o++)
+				{
+					if (!(tens == ones && ones == t && t == o))
+					{
+						putchar(tens);
+						putchar(ones);
+						putchar(' ');
+						putchar(t);
+						putchar(o);
+						if (!(tens == '9' && ones == '8') &&
+							(t == '9' && o == '9'))
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
+				}
+				o = '0';
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
