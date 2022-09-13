8536d21c374a6775bb64c3ed7760e95312a2bb5a
oussenimbohou <oussenimbohou@gmail.com>
2022-09-13 13:47:18 -0700

    Task 6 done

---
+++ 0x02-functions_nested_loops/6-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+  *main - starting point
+  *
+  *Return:Always 0 (success)
+  */
+int main(void)
+{
+	int r;
+	r =_abs(-1);
+	printf("%d\n", r);
+	return (0);
+}
