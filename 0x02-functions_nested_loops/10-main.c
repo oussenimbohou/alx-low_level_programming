0591833b6382e42aefa75df0a47f246677651d62
oussenimbohou <oussenimbohou@gmail.com>
2022-09-13 14:45:47 -0700

    Sum of 2 numbers

---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+  *main - starting point
+  *
+  *Return:Always 0 (success)
+  */
+int main(void)
+{
+	int r;
+	r =add(25,90);
+	printf("%d\n", r);
+	return (0);
+}
