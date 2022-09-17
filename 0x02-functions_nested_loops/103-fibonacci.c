4b4fc76c5a1edc472bc154e803b93a5611353260
oussenimbohou <oussenimbohou@gmail.com>
2022-09-17 20:15:19 +0100

    Hot fix task 103

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -10,17 +10,18 @@
 int main(void)
 {
 	int i;
-	unsigned long f1 = 1, f2 = 2, f = f1 + f2, sum = f2;
+	unsigned long long f1 = 1, f2 = 2, f = f1 + f2, sum = f2;
 
 	for (i = 1; f <= LIMIT; i++)
 	{
 		f1 = f2;
 		f2 = f;
 		f = f1 + f2;
-		if (f % 2 == 0)
+		if (f % 2 == 0 && f <= LIMIT)
 		{
+
 			sum += f;
 		}
 	}
-	printf("%lu\n", sum);
+	printf("%llu\n", sum);
 }
