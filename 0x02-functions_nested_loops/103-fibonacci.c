164adc50647d5dc0264b831c6a498e9d517b3c8a
oussenimbohou <oussenimbohou@gmail.com>
2022-09-17 20:19:43 +0100

    Hot fix task 103

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#define LIMIT 4000000
 
 /**
   *main - Starting of the program to print out the sum of
@@ -12,12 +11,12 @@ int main(void)
 	int i;
 	unsigned long long f1 = 1, f2 = 2, f = f1 + f2, sum = f2;
 
-	for (i = 1; f <= LIMIT; i++)
+	for (i = 1; f <= 4000000; i++)
 	{
 		f1 = f2;
 		f2 = f;
 		f = f1 + f2;
-		if (f % 2 == 0 && f <= LIMIT)
+		if (f % 2 == 0 && f <= 4000000)
 		{
 
 			sum += f;
