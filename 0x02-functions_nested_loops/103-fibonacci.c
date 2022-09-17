2bc8a9e5079bffcedbda8c1df3092d56059edc03
oussenimbohou <oussenimbohou@gmail.com>
2022-09-17 21:19:47 +0100

    Hot fix task 103

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#define LIMIT 4000000
 /**
   *main - Starting of the program to print out the sum of
   *the even fibonacci numbers less than 4000000.
@@ -11,16 +11,17 @@ int main(void)
 	int i;
 	long f1 = 1, f2 = 2, f = f1 + f2, sum = f2;
 
-	for (i = 1; f <= 4000000; i++)
+	for (i = 1; f <= LIMIT; i++)
 	{
 		f1 = f2;
 		f2 = f;
 		f = f1 + f2;
-		if (f % 2 == 0 && f <= 4000000)
+		if (f % 2 == 0 && f <= LIMIT)
 		{
 
 			sum += f;
 		}
 	}
 	printf("%ld\n", sum);
+	return (0);
 }
