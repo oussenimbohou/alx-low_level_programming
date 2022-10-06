7d1f457a19fa57e4ce18d86cfe4f44ed4766b5bd
oussenimbohou <oussenimbohou@gmail.com>
2022-10-07 00:09:50 +0100

    String concatenation

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -32,8 +32,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	p = malloc(sizeof(*p) * (len1 + limit + 1));
 	if (p == NULL)
 		return (NULL);
-	strcpy(p, s1);
-	i = len1;
+	for (i = 0; s1[i] != '\0'; i++)
+		p[i] = s1[i];
 	for (j = 0; j < limit; i++, j++)
 		*(p + i) = *(s2 + j);
 	*(p + i) = '\0';
